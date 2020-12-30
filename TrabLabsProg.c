cab35d3219a5f63a5c371596cc81cade1c7d2ed0
carvalho28 <carvalhoo287@gmail.com>
2020-12-30 20:52:39 +0000

    Funções ordemCrescente e medianaDosElementos concluída

--- TrabLabsProg.c
+++ TrabLabsProg.c
@@ -1,5 +1,5 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 
 #define N 18
 
@@ -27,31 +27,54 @@ int menuPrincipal(void)
 //Imprime o vetor por ordem crescente
 void ordemCrescente(int *vet)
 {
+    int i, j, aux;
 
+    for (i = 0; i < N; ++i)
+    {
+        for (j = i + 1; j < N; ++j)
+        {
+            if (vet[i] > vet[j])
+            {
+                aux = vet[i];
+                vet[i] = vet[j];
+                vet[j] = aux;
+            }
+        }
+    }
+
+    for (i = 0; i < N; ++i)
+    {
+        printf("O conjunto por ordem crescente é: ");
+        printf("%d, \n", vet[i]);
+    }
 }
 
 //Cálcula a mediana dos elementos do vetor
-/*void ou int*/ medianaDosElementos(int *vet)
+void medianaDosElementos(int *vet)
 {
+    int soma, mediana;
+    ordemCrescente(vet);
+    // 18 / 2 = 9;
+    
+    soma = (vet[9]+ vet[10]); // Como N é par, 18, e a sua metade é ímpar, a mediana é através da soma dos seus valores 'do meio'
+    mediana = soma / 2;
 
+    printf("A mediana do conjunto dado é: %d", mediana);
 }
 
 //Multiplica os elementos por 3
 void multiplicaPorTres(int *vet)
 {
-
 }
 
 //Cria um arrAuxiliar para armazenar os quádruplos e com 2 ciclos for, armazena tudo num arr[2][N]
 void matriz18Por2ComQuadruplos(int *vet)
 {
-
 }
 
 //Calcula o logaritmo de todos os elementos do vetor
 void logaritmoDosElementos(int *vet)
 {
-
 }
 
 //Valores dos elementos que estão em posições multiplas de 3
@@ -74,12 +97,12 @@ int main()
     {
         resposta_menu = menuPrincipal();
 
-        for(int i = 0; i < N; i++)   //Faz o vetor auxiliar igual ao do input para não estarmos a alterar o do input consecutivamente
+        for (int i = 0; i < N; i++) //Faz o vetor auxiliar igual ao do input para não estarmos a alterar o do input consecutivamente
         {
             vetAux[i] = vet[i];
         }
 
-        switch(resposta_menu)
+        switch (resposta_menu)
         {
         case 1:
             ordemCrescente(vetAux);
@@ -100,7 +123,7 @@ int main()
             valoresDasPosicoesMult3(vetAux);
         }
 
-    } while() //condição para repetir o menu
+    } while (1); //condição para repetir o menu
 
-    return 0;
+        return 0;
 }
