ce909227584472b00ce80d1de9774aedc65aa85e
carvalho28 <carvalhoo287@gmail.com>
2021-01-07 14:25:24 +0000

    Separação dos ficheiros - Edu

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#define N 18
+
+int main(void)
+{
+    printf("\e[1;1H\e[2J");
+    printf("Seja bem-vindo(a) ao trabalho do Grupo N!\n");
+    printf("\nInsira 18 números inteiros compreendidos entre 6 e 28:\n");
+
+    return 0;
+}
